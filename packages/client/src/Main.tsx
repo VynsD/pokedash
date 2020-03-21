be336fc947ae02c210a3048e62cd832111589120
Vincenzo Dell'Avvocato <vincenzo.dellavvocato@spindox.it>
2020-03-21 23:37:54 +0100

    REDO updated gitIgnore && MVD moved logo into assets

--- packages/client/src/Main.tsx
+++ packages/client/src/Main.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/styles/img/logo.svg';
 import * as ANTD from 'antd';
 import './assets/styles/Main.scss';
 
